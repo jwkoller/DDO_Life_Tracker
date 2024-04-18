f06792837a897f63b2e533110d11ff25f47214c8
jwkoller <jonwkoller@gmail.com>
2024-04-17 23:55:11 -0400

    Added MetroLog nuget and set up in dependency injection

--- DDO_Life_Tracker/ViewModels/MainViewModel.cs
+++ DDO_Life_Tracker/ViewModels/MainViewModel.cs
@@ -1,23 +1,29 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using DDO_Life_Tracker.Models;
+using MetroLog;
+using Microsoft.Extensions.Logging;
 using System.Collections.ObjectModel;
 
 namespace DDO_Life_Tracker.ViewModels
 {
     public partial class MainViewModel : ObservableObject
     {
-        public MainViewModel() 
-        { 
-            Incarnations = new ObservableCollection<Incarnation>();
-        }
-
         [ObservableProperty]
         private ObservableCollection<Incarnation> _incarnations;
+
+        private readonly ILogger<MainViewModel> _logger;
+
+        public MainViewModel(ILogger<MainViewModel> logger) 
+        { 
+            Incarnations = new ObservableCollection<Incarnation>();
+            _logger = logger;
+        }
 
         [RelayCommand]
         public void AddIncarnation()
         {
+            _logger.LogInformation($"Adding new incarnation");
             //TEST
             Incarnation newLife = new Incarnation(new Human(), new Monk(12));
             newLife.AddClass(new Fighter(6));
